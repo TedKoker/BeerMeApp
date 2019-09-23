d07f6a25b62ef19adb02e25ff3a3c90a1cc63ad4
unknown <tedkol1992@gmail.com>
2019-09-23 11:53:43 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, asNativeElements } from '@angular/core';
+import { Button } from 'protractor';
+import { resolve } from 'q';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  searchDone:boolean = false;
+  sizeOfInput: string = ""+window.innerWidth/5+"%";
+  sizeOfButton: string =""+(window.innerWidth/5)+"%";
   title = 'BeerMeApp';
+
+  ngOnInit(){
+  }
+
+  onWindowCange(){
+    if(!this.searchDone){
+      this.sizeOfInput=""+window.innerWidth/5+"%";
+      this.sizeOfButton =""+(window.innerWidth/5)+"%";
+    }
+    else{
+      this.sizeOfInput=""+(window.innerWidth/8.8)+"%";
+      this.sizeOfButton=""+(window.innerWidth/8.8)+"%";
+    }
+  }
+
+  async searchClicked(){
+    this.searchDone=true;
+    let i = 5;
+    //make the interval async await
+    let getSmaller = setInterval(()=>{
+      if(i<8.8){
+        i+=0.04;
+        this.sizeOfInput=""+(window.innerWidth/i)+"%";
+        this.sizeOfButton=""+(window.innerWidth/i)+"%";
+      }
+      else clearInterval(getSmaller);
+    },10);
+    await this.delay(1000);
+    console.log("end");
+    //this.sizeOfInput=""+window.innerWidth/8.8+"%";
+    //this.sizeOfButton=""+(window.innerWidth/8.8)+"%";
+  }
+
+  delay(ms: number) {
+    return new Promise( resolve => setTimeout(resolve, ms) );
+  }
 }
